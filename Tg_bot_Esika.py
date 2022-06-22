518cfe6c3556742fc9e243a736f8910d6db2a361
Nikita Shirokopetlev <35659452+FlameDanita@users.noreply.github.com>
2022-06-23 00:05:02 +0300

    add on/off function

--- Tg_bot_Esika.py
+++ Tg_bot_Esika.py
@@ -1,6 +1,14 @@
+import time, threading, schedule
 import telebot #pip install pyTelegramBotAPI
 import API
-import time, threading, schedule
+import emoji
+
+chat_ids = {"Danita" : 692791213,
+            "FlameDarya" : 539280636,
+            "Fobos" : 809542339} 
+sticker_ids = {"Хитрая_Миён" : "CAACAgIAAxkBAAEFFjBisMOr40zNs3hZ57qW8ySateZbuwAC0RUAAhl7sUiGNg3U3z0OIygE",
+               "Привет_Ариша" : "CAACAgIAAxkBAAEFFjJisMSiGLk0SwZGM6C4QE_LSMVk_wACYSQAAp7OCwABKLgYIbi2x0ooBA"}
+
 def start_bot():
     token = API.get_tg_TOKEN()
     bot = telebot.TeleBot(token, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
@@ -15,7 +23,49 @@ def start_bot():
 
     bot.infinity_polling()
 
-def stop_bot():
+def fobos_pc():
+    flag = True
     token = API.get_tg_TOKEN()
     bot = telebot.TeleBot(token, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
-    bot.stop_polling()
+    bot.send_message(chat_ids['Danita'], emoji.emojize('Эсика активированна, приветствую, дорогой друг :smirk::heartbeat:', language='alias'))
+    bot.send_sticker(chat_ids['Danita'], sticker_ids['Хитрая_Миён'])
+    bot.send_message(chat_ids['Danita'], emoji.emojize('Fobos PC complited :white_check_mark:', language='alias'))
+    bot.send_message(chat_ids['Fobos'], emoji.emojize('Fobos PC complited :white_check_mark:', language='alias'))
+
+    @bot.message_handler(commands=['off'])
+    def stop_command(message):
+        bot.reply_to(message, 'Выключаюсь...')
+        flag = False
+        time.sleep(1)
+        bot.stop_polling()
+
+    @bot.message_handler(func=lambda m: True)
+    def echo_all(message):
+        bot.reply_to(message, "Не знаю таких команд :/")
+    
+    if flag:
+        bot.infinity_polling(interval=1)
+
+def fobos_laptop():
+    flag = True
+    token = API.get_tg_TOKEN()
+    bot = telebot.TeleBot(token, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+    bot.send_message(chat_ids['Danita'], emoji.emojize('Эсика активированна, приветствую, дорогой друг :smirk::heartbeat:', language='alias'))
+    bot.send_sticker(chat_ids['Danita'], sticker_ids['Хитрая_Миён'])
+    bot.send_message(chat_ids['Danita'], emoji.emojize('Fobos laptop complited :white_check_mark:', language='alias'))
+    bot.send_message(chat_ids['Fobos'], emoji.emojize('Fobos laptop complited :white_check_mark:', language='alias'))
+
+    @bot.message_handler(commands=['off'])
+    def stop_command(message):
+        bot.reply_to(message, 'Выключаюсь...')
+        flag = False
+        time.sleep(1)
+        bot.stop_polling()
+
+    @bot.message_handler(func=lambda m: True)
+    def echo_all(message):
+        bot.reply_to(message, "Не знаю таких команд :/")
+
+    if flag:
+        bot.infinity_polling(interval=1)
+    
